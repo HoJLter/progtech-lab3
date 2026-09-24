6c17875024cec25e2434b491332d0dc36ff1ad70
HoJLter <hojlter.work@gmail.com>
2026-09-25 01:43:01 +0300

    Add: done

--- progtech-lab2/main.cpp
+++ progtech-lab2/main.cpp
@@ -1,20 +1,216 @@
-﻿// progtech-lab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#include <vector>
 #include <iostream>
+#include <fstream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+struct Node {
+	Node* left;
+	Node* right;
+	int value;
+};
+
+class BST {
+	Node* root;
+
+private:
+	Node* createNode(int value) {
+		Node* node = new Node();
+		node->left = nullptr;
+		node->right = nullptr;
+		node->value = value;
+
+		return node;
+	}
+
+	void insertRecursive(Node*& localRoot, int value) {
+		if (localRoot == nullptr) {
+			localRoot = createNode(value);
+			return;
+		}
+		if (value > localRoot->value)
+			insertRecursive(localRoot->right, value);
+		else {
+			insertRecursive(localRoot->left, value);
+		}
+	}
+
+	static void printRecursive(Node* localRoot) {
+		if (localRoot == nullptr) {
+			return;
+		}
+		printRecursive(localRoot->left);
+		std::cout << localRoot->value << " ";
+		printRecursive(localRoot->right);
+	}
+
+	bool isSubtreeHasNodes(Node* localRoot, const std::vector<int>& values) {
+		if (localRoot == nullptr) {
+			return false;
+		}
+
+		bool flag = false;
+		for (auto x : values) {
+			if (localRoot->value == x) {
+				flag = true;
+				break;
+			}
+		}
+		flag = flag || 
+			isSubtreeHasNodes(localRoot->left, values) || 
+			isSubtreeHasNodes(localRoot->right, values);
+		return flag;
+	}
+
+	void findSubtreesNotIncludesNodesRecursive(
+		Node* localRoot, 
+		const std::vector<int>& nodes, 
+		std::vector<Node*>& result
+	) 
+	{
+		if (localRoot == nullptr) {
+			return;
+		}
+
+		if (!isSubtreeHasNodes(localRoot, nodes)) {
+			result.push_back(localRoot);
+			return;
+		}
+
+		findSubtreesNotIncludesNodesRecursive(localRoot->left, nodes, result);
+		findSubtreesNotIncludesNodesRecursive(localRoot->right, nodes, result);
+	}
+
+	void deleteRecursive(Node* localRoot) {
+		if (localRoot == nullptr) {
+			return;
+		}
+
+		deleteRecursive(localRoot->left);
+		deleteRecursive(localRoot->right);
+
+		delete localRoot;
+	}
+
+public:
+	BST() {
+		root = nullptr;
+	}
+	
+	~BST() {
+		deleteRecursive(root);
+	}
+
+	static void print(Node* localRoot) {
+		printRecursive(localRoot);
+	}
+
+	void insert(int value) {
+		insertRecursive(root, value);
+	}
+
+	void print() {
+		printRecursive(root);
+	}
+
+	void printRoot() {
+		std::cout << root->value;
+	}
+
+	std::vector<Node*> findSubtreesNotIncludesNodes(const std::vector<int>& nodes) {
+		std::vector<Node*> result;
+
+		findSubtreesNotIncludesNodesRecursive(root, nodes, result);
+
+		return result;
+	}
+};
+
+void readValuesFromConsole(std::vector<int>& values, int count) {
+	for (int i = 0; i < count; i++) {
+		int value;
+		std::cin >> value;
+		values.push_back(value);
+	}
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void readValuesFromFile(std::vector<int>& values, std::string filename) {
+	std::ifstream file(filename);
+	if (!file) {
+		throw std::runtime_error("Can't open the file");
+	}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+	int count;
+	file >> count;
+
+	for (int i = 0; i < count; i++) {
+		int value;
+		file >> value;
+		values.push_back(value);
+	}
+}
+
+
+int main() {
+	while (true) {
+		BST tree;
+		std::cout << "Where do I get the tree? \n1 - console\n2 - file\n0 - exit\n\nEnter: ";
+		int switchValue;
+		std::cin >> switchValue;
+		std::vector<int> values;
+		switch (switchValue) {
+			case 1:
+			{
+				int count;
+				std::cout << "Enter the number of tree elements: ";
+				std::cin >> count;
+				std::cout << "Enter the elements of tree: ";
+				readValuesFromConsole(values, count);
+				break;
+			}
+			case 2: {
+				std::string filename;
+				std::cout << "Enter the filename: ";
+				std::cin >> filename;
+				readValuesFromFile(values, filename);
+				break;
+			}
+			case 0: {
+				return 0;
+			}
+		}
+		for (auto x : values) {
+			tree.insert(x);
+		}
+
+
+		std::cout << "Enter the count of values that must not be in the subtree: ";
+		int count;
+		std::cin >> count;
+		std::cout << "Enter a values that must not be in the subtree: ";
+		std::vector<int> valuesToNotInclude;
+		readValuesFromConsole(valuesToNotInclude, count);
+
+		std::vector<Node*> subtrees = tree.findSubtreesNotIncludesNodes(valuesToNotInclude);
+		
+		std::cout << "FOUND SUBTREES:";
+		std::cout << std::endl;
+		for (int i = 0; i < subtrees.size(); i++) {
+			std::cout << i << "." << " Root: " << subtrees[i]->value << " Subtree: ";
+			BST::print(subtrees[i]);
+			std::cout << std::endl;
+		}
+
+		std::cout << "WHOLE TREE: ";
+		std::cout << std::endl;
+		std::cout << "Root: ";
+		tree.printRoot();
+		std::cout << " Tree : ";
+		tree.print();
+
+		std::cout << std::endl;
+		std::cout << "\nPress Enter to continue...\n";
+		std::cin.ignore(30, '\n');
+		std::cin.get();
+		system("cls");
+	}
+}
+
